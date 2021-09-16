48dfa0d0e41eafa8afe406dc62beca6ef7d4a7a1
Engasht <the.engasht@gmail.com>
2021-09-16 13:39:47 +0430

    Add files via upload

---
+++ 005 - 01 - For Loops.py
@@ -0,0 +1,14 @@
+# Spyder Editor
+# Created on Thu Sep 16 11:32:28 2021
+# @author: Mani Mehrabi
+# Part of a Python Bootcamp - Beginner
+# 005 - 01 - for loops
+
+fruits = ["Apple", "Orange", "Grape", "Peach", "Pear"]
+
+for fruit in fruits:
+    print(fruit)
+    print(fruit + " Pie")
+print(fruits)
+
+
