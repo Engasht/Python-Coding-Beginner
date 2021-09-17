bf2e92f6d64e72557a0e5eec7fe674e84d534bcc
Engasht <the.engasht@gmail.com>
2021-09-17 21:09:55 +0430

    Add files via upload

---
+++ 006 - 01 - Functions.py
@@ -0,0 +1,17 @@
+# Spyder Editor
+# Created on Fri Sep 17 19:51:48 2021
+# @author: Mani Mehrabi
+# Part of a Python Bootcamp - Beginner
+# 006 - 01 - Functions
+
+print("Hello world!")
+char_num = len("Hello world!")
+print(char_num)
+
+def my_function():
+    print("Hello")
+    print("Bye")
+    
+my_function()
+
+# Check reeborg.ca
