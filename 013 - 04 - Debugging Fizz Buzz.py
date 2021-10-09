a3617e9d3e97e763744c86da06ac22093a90a687
Engasht <the.engasht@gmail.com>
2021-10-09 11:30:35 +0330

    Add files via upload

---
+++ 013 - 04 - Debugging Fizz Buzz.py
@@ -0,0 +1,25 @@
+# Spyder Editor
+# Created on Sat Oct  9 11:20:03 2021
+# @author: Mani Mehrabi
+# Part of a Python Bootcamp - Beginner
+# 013 - 04 - Debugging Fizz Buzz
+
+# for number in range(1, 101):
+#   if number % 3 == 0 or number % 5 == 0:
+#     print("FizzBuzz")
+#   if number % 3 == 0:
+#     print("Fizz")
+#   if number % 5 == 0:
+#     print("Buzz")
+#   else:
+#     print([number])
+    
+for number in range(1, 101):
+  if number % 3 == 0 and number % 5 == 0:
+    print("FizzBuzz")
+  elif number % 3 == 0:
+    print("Fizz")
+  elif number % 5 == 0:
+    print("Buzz")
+  else:
+    print([number])
