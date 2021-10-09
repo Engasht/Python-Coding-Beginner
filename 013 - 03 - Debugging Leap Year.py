951dcb5fb8f9524a2edc9b8926e51020770aa534
Engasht <the.engasht@gmail.com>
2021-10-09 11:21:48 +0330

    Add files via upload

---
+++ 013 - 03 - Debugging Leap Year.py
@@ -0,0 +1,32 @@
+# Spyder Editor
+# Created on Sat Oct  9 11:16:06 2021
+# @author: Mani Mehrabi
+# Part of a Python Bootcamp - Beginner
+# 013 - 03 - Debugging Leap Year
+
+# year = input("Which year do you want to check?")
+
+# if year % 4 == 0:
+#   if year % 100 == 0:
+#     if year % 400 == 0:
+#       print("Leap year.")
+#     else:
+#       print("Not leap year.")
+#   else:
+#     print("Leap year.")
+# else:
+#   print("Not leap year.")
+  
+year = int(input("Which year do you want to check? "))
+
+if year % 4 == 0:
+  if year % 100 == 0:
+    if year % 400 == 0:
+      print("Leap year.")
+    else:
+      print("Not leap year.")
+  else:
+    print("Leap year.")
+else:
+  print("Not leap year.")
+  
