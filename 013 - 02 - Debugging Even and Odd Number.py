5ddae0ce243544480cc81916dcbd6b172f27241f
Engasht <the.engasht@gmail.com>
2021-10-09 11:15:21 +0330

    Add files via upload

---
+++ 013 - 02 - Debugging Even and Odd Number.py
@@ -0,0 +1,19 @@
+# Spyder Editor
+# Created on Sat Oct  9 11:09:04 2021
+# @author: Mani Mehrabi
+# Part of a Python Bootcamp - Beginner
+# 013 - 02 - Debugging Exercise 1
+
+# number = int(input("Which number do you want to check? "))
+
+# if number % 2 = 0:
+#   print("This is an even number.")
+# else:
+#   print("This is an odd number.")
+
+number = int(input("Which number do you want to check? "))
+
+if number % 2 == 0:
+  print("This is an even number.")
+else:
+  print("This is an odd number.")
